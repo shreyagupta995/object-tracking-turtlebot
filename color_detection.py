9a6deb135ff97cb262347465390416ed97a8abe8
Shreya <shreyagupta995@gmail.com>
2016-04-06 12:41:57 -0500

    Adding functional color detection

--- color_detection.py
+++ color_detection.py
@@ -14,44 +14,31 @@ roslib.load_manifest('bb8')
 class image_converter:
 
     def __init__(self):
-        self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=3)
-
+        self.image_pub = rospy.Publisher("bb8",Image,queue_size=3)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
 
     def callback(self,data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            #cv_image = cv2.cvtColor(cv_image,cv.CV_BGR2GRAY)
-            #cv_image = cv2.medianBlur(cv_image,3)
+            # cv_image = cv2.cvtColor(cv_image,cv.CV_BGR2HSV)
         except CvBridgeError as e:
             print(e)
 
-        # construct the argument parse and parse the arguments
-        # ap = argparse.ArgumentParser()
-        # ap.add_argument("-i", "--image", help = "path to the image")
-        # args = vars(ap.parse_args())
-
-        # # load the image
-        # image = cv2.imread(args["image"])
-
         # define the list of boundaries
-        boundaries = [([86, 31, 4], [220, 88, 50])]
+        # Modified BGR blue [([102, 51, 0], [255, 153, 51])]
+        # Original BGR blue [([86, 31, 4], [220, 88, 50])]
 
-        # loop over the boundaries
-        for (lower, upper) in boundaries:
-            # create NumPy arrays from the boundaries
-            lower = np.array(lower, dtype = "uint8")
-            upper = np.array(upper, dtype = "uint8")
+        # HSV blue
+        lower_blue = np.array([102, 51, 0]) # Orginal HSV lower:  Modified HSV lower: [210,100,40]
+        upper_blue = np.array([255, 153, 51]) # Original HSV upper:  Modified HSV upper: [21,80,100]
 
-            # find the colors within the specified boundaries and apply
-            # the mask
-            mask = cv2.inRange(cv_image, lower, upper)
-            output = cv2.bitwise_and(cv_image, cv_image, mask = mask)
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(cv_image, lower_blue, upper_blue)
 
-        # show the images
-        # cv2.imshow("images", np.hstack([cv_image, output]))
-        # cv2.waitKey(0)
+        # Bitwise-AND mask and original image
+        # find the colors within the specified boundaries and apply the mask
+        output = cv2.bitwise_and(cv_image,cv_image, mask=mask)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(output, "bgr8"))
@@ -59,8 +46,8 @@ class image_converter:
             print(e)
 
 def main(args):
-    ic = image_converter()
     rospy.init_node('image_converter', anonymous=True)
+    ic = image_converter()
     try:
         rospy.spin()
     except KeyboardInterrupt:
